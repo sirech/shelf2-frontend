149521ab2d0daac5b2b22eeb25bca6cdff50b3c4
Mario Fernandez <mario.fernandez@xing.com>
2017-05-30 19:33:04 +0200

    Add redux router

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,29 @@
 import React, { Component } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
 import logo from './logo.svg'
 import './App.css'
 
 class App extends Component {
   render () {
     return (
-      <div className='App'>
-        <div className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <h2>Welcome to React</h2>
+      <Router>
+        <div className='App'>
+          <div className='App-header'>
+            <img src={logo} className='App-logo' alt='logo' />
+            <h2>Welcome to React</h2>
+          </div>
+          <Route exact path='/' component={Home} />
         </div>
-        <p className='App-intro'>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </Router>
     )
   }
 }
+
+const Home = () => (
+  <p className='App-intro'>
+    To get started, edit <code>src/App.js</code> and save to reload.
+  </p>
+)
 
 export default App
