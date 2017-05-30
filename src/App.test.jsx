a77c73d90af86cca6e3d9315e082c333431cea4a
Mario Fernandez <mario.fernandez@xing.com>
2017-05-30 23:20:28 +0200

    Switch test

---
+++ src/App.test.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { fullRender } from './test'
+import App from './App'
+
+describe('components', () => {
+  describe('App', () => {
+    it('renders correctly', () => {
+      const { component } = fullRender(<App />)
+      expect(component.toJSON()).toMatchSnapshot()
+    })
+  })
+})
