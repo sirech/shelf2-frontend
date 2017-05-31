5d9592c7672425eba52fe5e67f838940a5536493
Mario Fernandez <mario.fernandez@xing.com>
2017-05-31 20:12:09 +0200

    Proper routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Redirect, Route } from 'react-router-dom'
 
 import { Col, Container, Row } from 'reactstrap'
 
 import Header from './molecules/header'
+import BookList from './organisms/book_list'
 import Navigation from './organisms/navigation'
+
+const year = new Date().getFullYear()
 
 class App extends Component {
   render () {
@@ -13,7 +16,10 @@ class App extends Component {
         <div>
           <Header />
           <Container fluid>
-            <Route path='/' component={Home} />
+            <Route exact path='/books' render={() => (
+              <Redirect to={`/books/${year}`} />
+            )} />
+            <Route exact path='/books/:year' component={Home} />
           </Container>
         </div>
       </Router>
@@ -23,6 +29,9 @@ class App extends Component {
 
 const Home = () => (
   <Row>
+    <Col sm='8' md='9' lg='10'>
+      <BookList />
+    </Col>
     <Col sm='4' md='3' lg='2'>
       <Navigation />
     </Col>
