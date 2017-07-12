daf9d3a63736b3c0458352c433baf6d6391fa5e1
Mario Fernandez <mario.fernandez@xing.com>
2017-07-12 23:05:04 +0200

    Basic state

---
+++ src/state/books/index.js
@@ -0,0 +1,5 @@
+import * as constants from './constants'
+import * as actions from './actions'
+
+export { actions, constants }
+export { default as reducer } from './reducers'
--- src/state/index.js
+++ src/state/index.js
@@ -1,6 +1,9 @@
 import { combineReducers } from 'redux'
 
+import { constants as booksConstants, reducer as booksReducer } from './books'
+
 const rootReducer = combineReducers({
+  [booksConstants.namespace]: booksReducer
 })
 
 export default rootReducer
