eadc1ce56b8b76e61b9513979cdf96b371ee6b02
Mario Fernandez <mario.fernandez@thoughtworks.com>
2017-11-03 17:18:12 +0100

    Make tests resistant against timeouts

--- src/state/books/__tests__/actions.test.js
+++ src/state/books/__tests__/actions.test.js
@@ -13,35 +13,41 @@ import { books, rest } from '../../__fixtures__'
 describe('actions', () => {
   let store
   let year = '2016'
+
   const provider = createProvider()
 
-  beforeAll(done => provider.setup().then(done))
-  afterAll(done => provider.verify().then(provider.finalize).then(done))
+  beforeAll(async () => {
+    await provider.setup()
+
+    const interaction = {
+      state: 'i have an empty state',
+      uponReceiving: 'a request for a list of books for a given year',
+      withRequest: {
+        method: 'GET',
+        path: '/rest/books',
+        query: { year },
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'X-Requested-With': 'XMLHttpRequest'
+        }
+      },
+      willRespondWith: {
+        status: 200,
+        headers: { 'Content-Type': 'application/json' },
+        body: rest.books
+      }
+    }
+
+    return provider.addInteraction(interaction)
+  }, 5 * 60 * 1000)
+
+  afterAll(async () => {
+    await provider.verify()
+    return provider.finalize()
+  }, 5 * 60 * 1000)
 
   describe('fetchBooks', () => {
-    beforeAll(done => {
-      const interaction = {
-        state: 'i have an empty state',
-        uponReceiving: 'a request for a list of books for a given year',
-        withRequest: {
-          method: 'GET',
-          path: '/rest/books',
-          query: { year },
-          headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            'X-Requested-With': 'XMLHttpRequest'
-          }
-        },
-        willRespondWith: {
-          status: 200,
-          headers: { 'Content-Type': 'application/json' },
-          body: rest.books
-        }
-      }
-      provider.addInteraction(interaction).then(done, done)
-    })
-
     beforeEach(() => {
       store = mockStore({})
     })
