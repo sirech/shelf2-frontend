28afdedc3f1eeb41f3b51cb38754fb0e4aedbf93
Mario Fernandez <mario.fernandez@thoughtworks.com>
2017-11-14 18:28:31 +0100

    fix navigation bar

--- src/molecules/navigation_item/NavigationItem.jsx
+++ src/molecules/navigation_item/NavigationItem.jsx
@@ -5,7 +5,7 @@ import { Badge, ListGroupItem } from 'reactstrap'
 import { LinkContainer } from 'react-router-bootstrap'
 
 const NavigationItem = ({year, count}: { year: number, count: number}) => (
-  <LinkContainer to={`/books/${year}`}>
+  <LinkContainer to={`/books/${year}`} className='d-flex justify-content-between align-items-center'>
     <ListGroupItem action tag='a'>
       <span>{year}</span >
       <Badge pill color='warning' className='ml-auto'>{count}</Badge>
