64a4bfd4a481f2644a83eb8530aafc7b7d2a80fe
Mario Fernandez <mario.fernandez@thoughtworks.com>
2018-03-26 23:03:18 +0200

    Add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,18 +2,19 @@ module.exports = {
   env: {
     es6: true,
     mocha: true,
-    "jest/globals": true
+    'jest/globals': true,
   },
-  "extends": [
-    "standard",
-    "standard-react",
-    "plugin:flowtype/recommended"
+  extends: [
+    'standard',
+    'standard-react',
+    'plugin:flowtype/recommended',
+    'prettier',
+    'prettier/flowtype',
+    'prettier/react',
+    'prettier/standard',
   ],
-  "plugins": [
-    "jest",
-    "flowtype"
-  ],
-  "globals": {
-    "localStorage": true
-  }
+  plugins: ['jest', 'flowtype'],
+  globals: {
+    localStorage: true,
+  },
 }
