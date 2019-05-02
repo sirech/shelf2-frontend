ae9a3b2a8adbb9e1d8e7c21110a61cdb2b7797b2
Mario Fernandez <mario.fernandez@thoughtworks.com>
2019-05-03 00:03:47 +0200

    Add validation for form

--- src/organisms/new_book/NewBook.jsx
+++ src/organisms/new_book/NewBook.jsx
@@ -8,6 +8,7 @@ import { Button, Container, Row, Col } from 'reactstrap'
 import ReactstrapSelect from './ReactstrapSelect'
 import input from './input'
 import { Field, Form, Formik } from 'formik'
+import * as Yup from 'yup'
 
 import type { BookForm as Book } from 'types'
 
@@ -21,15 +22,31 @@ const initialBook: Book = {
 
 const categories = ['sociology', 'software', 'econ', 'history', 'other']
 
+const bookSchema = Yup.object().shape({
+  title: Yup.string()
+    .min(5, 'min length 5')
+    .max(200, 'max length 200')
+    .required('Required'),
+  year: Yup.number()
+    .min(2000, 'starting at 2000')
+    .max(2100, 'ending at 2100')
+    .required('Required'),
+  stars: Yup.number()
+    .min(1, 'min 1 star')
+    .max(5, 'max 5 stars')
+    .required('Required'),
+})
+
 const NewBookWrapper = () => (
   <Formik
     initialValues={initialBook}
     render={NewBook}
+    validationSchema={bookSchema}
     onSubmit={values => console.log(values)}
   />
 )
 
-const NewBook = () => (
+const NewBook = isSubmitting => (
   <section>
     <Form>
       <Container fluid>
@@ -63,7 +80,12 @@ const NewBook = () => (
               </Button>
             </LinkContainer>
 
-            <Button className="float-right mr-2" color="primary">
+            <Button
+              className="float-right mr-2"
+              color="primary"
+              type="submit"
+              disabled={!isSubmitting}
+            >
               Create
             </Button>
           </Col>
