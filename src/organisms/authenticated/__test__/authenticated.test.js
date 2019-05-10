b596ccfad83d590ec3de4ab4944a18741a385a90
Mario Fernandez <mario.fernandez@thoughtworks.com>
2019-05-10 17:01:13 +0200

    more meaningful authenticated test

--- src/organisms/authenticated/__test__/authenticated.test.js
+++ src/organisms/authenticated/__test__/authenticated.test.js
@@ -7,22 +7,25 @@ import authenticated from '../authenticated'
 
 describe('authenticated', () => {
   // eslint-disable-next-line
-  const Dummy = ({ test }) => (
-    <div>
-      Mock
-      {test}
-    </div>
-  )
+  const Dummy = () => <div>Mock</div>
 
   it('renders the component if authenticated is required', () => {
     const Component = authenticated()(Dummy)
-    const { component } = fullRender(<Component test="stuff" />, state())
+    const { component } = fullRender(<Component />, state())
     expect(component.toJSON()).toMatchSnapshot()
+  })
+
+  it('does not render the component if authenticated is required and authentincation is not set', () => {
+    const currentState = state()
+    currentState.login.authenticated = false
+    const Component = authenticated()(Dummy)
+    const { component } = fullRender(<Component />, currentState)
+    expect(component.toJSON()).toBeNull()
   })
 
   it('does not render the component if authenticated is not required', () => {
     const Component = authenticated(false)(Dummy)
-    const { component } = fullRender(<Component test="stuff" />, state())
-    expect(component.toJSON()).toMatchSnapshot()
+    const { component } = fullRender(<Component />, state())
+    expect(component.toJSON()).toBeNull()
   })
 })
