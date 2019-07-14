cf6faf01f3c6bba437dbd777f583d88ea0afa32d
Mario Fernandez <mario.fernandez@thoughtworks.com>
2019-07-14 13:47:02 +0200

    switch to state hook

--- src/molecules/header/Header.jsx
+++ src/molecules/header/Header.jsx
@@ -1,6 +1,6 @@
 // @flow
 
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { LinkContainer } from 'react-router-bootstrap'
 import { Helmet } from 'react-helmet'
@@ -26,53 +26,29 @@ const Login = authenticated(false)(() => (
   </NavItem>
 ))
 
-type State = {
-  isOpen: boolean,
-}
-
-class Header extends React.Component {
-  state: State
-
-  toggle: Function
-
-  constructor() {
-    super()
-
-    this.toggle = this.toggle.bind(this)
-    this.state = {
-      isOpen: false,
-    }
-  }
-
-  toggle() {
-    this.setState({
-      isOpen: !this.state.isOpen,
-    })
-  }
-
-  render() {
-    return (
-      <header className="mb-4">
-        <Helmet>
-          <meta charSet="utf-8" />
-          <title>Shelf</title>
-        </Helmet>
-        <Navbar dark color="dark" expand="sm">
-          <LinkContainer to="/books">
-            <NavbarBrand>Shelf</NavbarBrand>
-          </LinkContainer>
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-              <SearchBar />
-              <Login />
-              <Logout />
-            </Nav>
-          </Collapse>
-        </Navbar>
-      </header>
-    )
-  }
+const Header = () => {
+  const [isOpen, setIsOpen] = useState(false)
+  return (
+    <header className="mb-4">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Shelf</title>
+      </Helmet>
+      <Navbar dark color="dark" expand="sm">
+        <LinkContainer to="/books">
+          <NavbarBrand>Shelf</NavbarBrand>
+        </LinkContainer>
+        <NavbarToggler onClick={() => setIsOpen(!isOpen)} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <SearchBar />
+            <Login />
+            <Logout />
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </header>
+  )
 }
 
 export default Header
