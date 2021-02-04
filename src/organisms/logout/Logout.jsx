5dcf25301e5cdba31ed049a6f84528dca767dc42
Mario Fernandez <mario.fernandez@hceris.com>
2021-02-04 15:52:36 +0100

    use hooks for logout

--- src/organisms/logout/Logout.jsx
+++ src/organisms/logout/Logout.jsx
@@ -1,42 +1,28 @@
 // @flow
 
 import React from 'react'
-import { connect } from 'react-redux'
+import { useDispatch } from 'react-redux'
 
 import { NavItem } from 'reactstrap'
 
 import authenticated from 'organisms/authenticated'
 
-import { actionPicker } from 'state'
 import { actions } from 'state/login'
 
-type Props = {
-  logout: (void) => void,
+const Logout = () => {
+  const dispatch = useDispatch()
+
+  return (
+    <NavItem>
+      <div
+        onClick={() => dispatch(actions.logout())}
+        className="nav-link"
+        role="link"
+      >
+        Logout
+      </div>
+    </NavItem>
+  )
 }
 
-class Logout extends React.Component<Props> {
-  handleClick: Function
-
-  constructor() {
-    super()
-    this.handleClick = this.handleClick.bind(this)
-  }
-
-  handleClick() {
-    this.props.logout()
-  }
-
-  render() {
-    return (
-      <NavItem>
-        <div onClick={this.handleClick} className="nav-link" role="link">
-          Logout
-        </div>
-      </NavItem>
-    )
-  }
-}
-
-export default authenticated()(
-  connect(null, actionPicker(['logout'])(actions))(Logout)
-)
+export default authenticated()(Logout)
