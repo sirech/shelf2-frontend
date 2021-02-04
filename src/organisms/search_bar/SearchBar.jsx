4d14b7ecd43919e61a2696edfa12564f173f88a9
Mario Fernandez <mario.fernandez@hceris.com>
2021-02-04 15:39:31 +0100

    refactor search bar to use hooks

--- src/organisms/search_bar/SearchBar.jsx
+++ src/organisms/search_bar/SearchBar.jsx
@@ -1,34 +1,23 @@
 // @flow
 
 import React from 'react'
-import { withRouter } from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
 
 import { Form, Input } from 'reactstrap'
 
-type Props = { history: Object }
-
-class SearchBar extends React.Component {
-  props: Props
-
-  handleChange: Function
-
-  constructor() {
-    super()
-    this.handleChange = this.handleChange.bind(this)
-  }
-
-  handleChange(e) {
-    e.preventDefault()
-    this.props.history.push(`/books/search/${e.target.value}`)
-  }
-
-  render() {
-    return (
-      <Form inline>
-        <Input onChange={this.handleChange} />
-      </Form>
-    )
-  }
+const handleChange = (e, history) => {
+  e.preventDefault()
+  history.push(`/books/search/${e.target.value}`)
 }
 
-export default withRouter(SearchBar)
+const SearchBar = () => {
+  const history = useHistory()
+
+  return (
+    <Form inline>
+      <Input onChange={(e) => handleChange(e, history)} />
+    </Form>
+  )
+}
+
+export default SearchBar
