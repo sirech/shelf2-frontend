f67824c4281c334bd6b216c0b23fe0cad0ea3bff
Mario Fernandez <mario.fernandez@hceris.com>
2025-12-27 13:56:53 +0100

    move pact back to v2 for compatibility

--- src/test/create_provider.ts
+++ src/test/create_provider.ts
@@ -5,7 +5,7 @@ const provider = (): Pact =>
   new Pact({
     port: 8989,
     dir: path.resolve(process.cwd(), 'pacts'),
-    spec: SpecificationVersion.SPECIFICATION_VERSION_V4,
+    spec: SpecificationVersion.SPECIFICATION_VERSION_V2,
     consumer: 'React',
     provider: 'Backend',
   })
